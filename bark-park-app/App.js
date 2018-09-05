20811e7e15a42c0128d353919d3556fa13555edb
pbchun <pbchun12@gmail.com>
2018-09-05 15:17:20 -0600

    removed header logo and footer from app.js

--- bark-park-app/App.js
+++ bark-park-app/App.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import Home from './components/Home'
-import FooterTabs from './components/FooterTabs'
-import HeaderLogo from './components/HeaderLogo'
 import Login from './components/Login'
 import CheckIn from './components/CheckIn'
 import CheckOut from './components/CheckOut'
